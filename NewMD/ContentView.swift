a5d7259561cfef41bbf179eef9468d2a7af63578
HACO <jasonytonlinecomeandsee@gmail.com>
2023-09-08 09:00:04 +0800

    Fixed Keyboard issue and bump version to 1.2.0

--- NewMD/ContentView.swift
+++ NewMD/ContentView.swift
@@ -27,10 +27,27 @@ struct WebView: UIViewRepresentable {
     let urlString: String
 
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        webView.navigationDelegate = context.coordinator
-        return webView
-    }
+            let webConfig = WKWebViewConfiguration()
+            
+            // Your existing configurations
+            let preferences = WKPreferences()
+            preferences.javaScriptCanOpenWindowsAutomatically = false
+            webConfig.preferences = preferences
+            
+            let webView = WKWebView(frame: .zero, configuration: webConfig)
+            webView.navigationDelegate = context.coordinator
+            
+            // Your existing configurations for zooming
+            webView.scrollView.isScrollEnabled = true
+            webView.scrollView.bouncesZoom = false
+            webView.scrollView.minimumZoomScale = 1.0
+            webView.scrollView.maximumZoomScale = 1.0
+
+            // Add observers for keyboard frame change
+            NotificationCenter.default.addObserver(context.coordinator, selector: #selector(context.coordinator.keyboardWillChangeFrame), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+            return webView
+        }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
         if let url = URL(string: urlString) {
@@ -43,22 +60,53 @@ struct WebView: UIViewRepresentable {
         Coordinator(self)
     }
 
-    class Coordinator: NSObject, WKNavigationDelegate {
+    class Coordinator: NSObject, WKNavigationDelegate, UIScrollViewDelegate {
         var parent: WebView
+        var webView: WKWebView?
 
         init(_ parent: WebView) {
             self.parent = parent
         }
 
-        // Implement WKNavigationDelegate methods if needed
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            // Your existing code
+            let script = "var meta = document.createElement('meta'); meta.name = 'viewport'; meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'; document.getElementsByTagName('head')[0].appendChild(meta);"
+            webView.evaluateJavaScript(script, completionHandler: nil)
+            self.webView = webView
+        }
+
+        @objc func keyboardWillChangeFrame(notification: NSNotification) {
+            guard let userInfo = notification.userInfo else { return }
+            guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+            
+            if keyboardFrame.origin.y == UIScreen.main.bounds.height {
+                // Keyboard is hidden
+                // Reset any previous adjustments (if necessary)
+                let resetJS = "window.scrollTo(0, 0);"
+                webView?.evaluateJavaScript(resetJS, completionHandler: nil)
+            } else {
+                // Keyboard is shown
+                // Execute JavaScript to scroll the web page to the active input element
+                let js = "var focusedElement = document.activeElement; focusedElement.scrollIntoView({behavior: 'smooth'});"
+                webView?.evaluateJavaScript(js, completionHandler: nil)
+            }
+        }
+
+        
+        deinit {
+            // Remove the observers when the coordinator is deinitialized
+            NotificationCenter.default.removeObserver(self)
+        }
     }
 }
 
 struct ContentView: View {
     var body: some View {
         WebView(urlString: "https://newmd.eu.org")
-//            .edgesIgnoringSafeArea(.bottom)
             .resignKeyboardOnDragGesture()
+            .navigationBarTitleDisplayMode(.inline)
+            .animation(nil)
+        
     }
 }
 
